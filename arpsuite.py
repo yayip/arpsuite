1537249caa414efdc68ff024eb740cf1123f8115
yayip <39186995+yayip@users.noreply.github.com>
2021-11-25 20:55:13 +0700

    Update arpsuite.py

--- arpsuite.py
+++ arpsuite.py
@@ -18,11 +18,11 @@ def add_rules():
 	subprocess.Popen("sysctl net.ipv4.ip_forward=1", stdout=subprocess.DEVNULL, shell=True)
 	# firewall redirection to burp
 	os.system("iptables -A FORWARD --in-interface " + options.iface + " -j ACCEPT")
-	os.system("iptables -t nat -A PREROUTING -i " + options.iface + " -p tcp -d " + options.spip + " -m multiport --dport " + options.dport + " -j REDIRECT --to-port " + options.bport)
+	os.system("iptables -t nat -A PREROUTING -i " + options.iface + " -p tcp -m multiport --dport " + options.dport + " -j REDIRECT --to-port " + options.bport)
 
 def remove_rules():
 	os.system("iptables -D FORWARD --in-interface " + options.iface + " -j ACCEPT")
-	os.system("iptables -D PREROUTING -t nat -i " + options.iface + " -p tcp -d " + options.spip + " -m multiport --dport " + options.dport + " -j REDIRECT --to-port " + options.bport)
+	os.system("iptables -D PREROUTING -t nat -i " + options.iface + " -p tcp -m multiport --dport " + options.dport + " -j REDIRECT --to-port " + options.bport)
 
 def get_gateway():
 	try: 
